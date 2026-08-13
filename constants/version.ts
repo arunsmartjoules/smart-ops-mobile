67f7b33c5cafe301bab601763d9bff2e92e080c3
arunsmartjoules <arun.kumar@smartjoules.in>
2026-08-13 13:01:55 +0530

    feat: Update app version to 1.1.1

--- constants/version.ts
+++ constants/version.ts
@@ -1,5 +1,5 @@
 
-export const APP_VERSION = "1.1.0";
+export const APP_VERSION = "1.1.1";
 
 /** Human-facing version string shown in the UI. */
 export const APP_VERSION_DISPLAY = `${APP_VERSION} (Beta)`;
