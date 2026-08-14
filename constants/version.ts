7980182cfef9cd40987b29eb685582e5f25ff164
arunsmartjoules <arun.kumar@smartjoules.in>
2026-08-14 11:01:34 +0530

    feat: Bump app version to 1.1.2

--- constants/version.ts
+++ constants/version.ts
@@ -1,5 +1,5 @@
 
-export const APP_VERSION = "1.1.1";
+export const APP_VERSION = "1.1.2";
 
 /** Human-facing version string shown in the UI. */
 export const APP_VERSION_DISPLAY = `${APP_VERSION} (Beta)`;
